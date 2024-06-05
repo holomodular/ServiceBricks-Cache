16802c3aa8ee86d08719a4d549711017e1053696
Danny Logsdon <danny.logsdon@holomodular.com>
2024-06-05 01:43:09 -0400

    Release v1.0.10

--- src/V1/ServiceBricks.Cache/Controllers/CacheDataApiController.cs
+++ src/V1/ServiceBricks.Cache/Controllers/CacheDataApiController.cs
@@ -1,10 +1,4 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-
-using System;
-using System.Collections.Generic;
-using System.Net;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 
 namespace ServiceBricks.Cache
